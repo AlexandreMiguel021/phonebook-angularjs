7459354a7a6fa34da23e38560978bb355637f5ac
Alexandre <alexandremigueldev@outlook.com>
2022-11-29 16:56:24 -0300

    update: add mock de operadoras para fake api

---
+++ api/data/operadoras.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: 'Oi', code: 14, category: 'Celular' },
+  { name: 'Vivo', code: 15, category: 'Celular' },
+  { name: 'Tim', code: 41, category: 'Celular' },
+  { name: 'GVT', code: 25, category: 'Fixo' },
+  { name: 'Embratel', code: 21, category: 'Fixo' }
+]
